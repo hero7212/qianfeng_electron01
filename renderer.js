19f735bbfa521609eec79b975e7a160be28b106b
frontendgod <3054750147@qq.com>
2020-02-22 19:36:01 +0800

    p05

--- renderer.js
+++ renderer.js
@@ -1,6 +1,10 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // No Node.js APIs are available in this process because
-// `nodeIntegration` is turned off. Use `preload.js` to
-// selectively enable features needed in the rendering
-// process.
+const electron = require('electron')
+
+function getProcessInfo() {
+    console.log('getCPUUsage:', process.getCPUUsage())
+    console.log('env:', process.env)
+    console.log('arch:', process.arch)
+}
